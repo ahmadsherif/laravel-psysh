ca9e4f453ce8bf2ea1bdcacabb2cca3fc84b1255
Ahmad Sherif <me@ahmadsherif.com>
2013-11-17 09:42:32 +0200

    Remove overridden PsySH method as it was fixed upstream

--- src/LaravelPsySH/Presenters/EloquentPresenter.php
+++ src/LaravelPsySH/Presenters/EloquentPresenter.php
@@ -13,20 +13,6 @@ class EloquentPresenter extends ObjectPresenter {
     return $value instanceof Model;
   }
 
-  // Copied directly from PsySH codebase to allow using of our getProperties
-  protected function presentValue($value, $depth = null)
-  {
-    if ($depth === 0)
-    {
-      return $this->presentRef($value);
-    }
-
-    $class = new \ReflectionObject($value);
-    $props = $this->getProperties($value, $class); // Was $props = self::getProperties($value, $class);
-
-    return sprintf('%s %s', $this->presentRef($value), $this->formatProperties($props));
-  }
-
   protected function getProperties($value, \ReflectionClass $class)
   {
     return $value->getAttributes();
